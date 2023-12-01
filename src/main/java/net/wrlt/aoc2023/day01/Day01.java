b807d2282125eff4beac6723102fb0c78308f8e1
Juan Parera <1420988+jparera@users.noreply.github.com>
2023-12-01 10:11:26 +0100

    Implement day 1 puzzle solution

---
+++ src/main/java/net/wrlt/aoc2023/day01/Day01.java
@@ -0,0 +1,95 @@
+package net.wrlt.aoc2023.day01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class Day01 {
+    public static class Part1 {
+        public static int execute(Path input) throws IOException {
+            try (var lines = parse(input)) {
+                return lines.mapToInt(Part1::valueOf).sum();
+            }
+        }
+
+        private static int valueOf(String line) {
+            int first = firstDigit(line);
+            int last = lastDigit(line);
+            return first * 10 + last;
+        }
+
+        private static int firstDigit(String input) {
+            for (int i = 0; i < input.length(); i++) {
+                if (Character.isDigit(input.charAt(i))) {
+                    return input.charAt(i) - '0';
+                }
+            }
+            return -1;
+        }
+
+        private static int lastDigit(String input) {
+            for (int i = input.length() - 1; i >= 0; i--) {
+                if (Character.isDigit(input.charAt(i))) {
+                    return input.charAt(i) - '0';
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static class Part2 {
+        private static final String[] NAMES = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine" };
+
+        public static int execute(Path input) throws IOException {
+            try (var lines = parse(input)) {
+                return lines.mapToInt(Part2::valueOf).sum();                
+            }
+        }
+
+        private static int valueOf(String line) {
+            int first = firstDigit(line);
+            int last = lastDigit(line);
+            return first * 10 + last;
+        }
+
+        private static int firstDigit(String input) {
+            for (int i = 0; i < input.length(); i++) {
+                var digit = digit(input, i);
+                if (digit != -1) {
+                    return digit;
+                }
+            }
+            return -1;
+        }
+
+        private static int lastDigit(String input) {
+            for (int i = input.length() - 1; i >= 0; i--) {
+                var digit = digit(input, i);
+                if (digit != -1) {
+                    return digit;
+                }
+            }
+            return -1;
+        }
+
+        private static int digit(String input, int index) {
+            var c = input.charAt(index);
+            if (Character.isDigit(c)) {
+                return c - '0';
+            }
+            for (int i = 1; i < NAMES.length; i++) {
+                if (input.startsWith(NAMES[i], index)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    private static Stream<String> parse(Path input) throws IOException {
+        return Files.lines(input);
+    }
+}
