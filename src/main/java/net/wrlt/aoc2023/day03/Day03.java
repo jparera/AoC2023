f277384d41a625a12fbdb63aeeb89849e7139acb
Juan Parera <1420988+jparera@users.noreply.github.com>
2023-12-03 21:27:32 +0100

    Parallelize Day 3 puzzle solution

--- src/main/java/net/wrlt/aoc2023/day03/Day03.java
+++ src/main/java/net/wrlt/aoc2023/day03/Day03.java
@@ -4,17 +4,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day03 {
     public static class Part1 {
         public static int execute(Path input) throws IOException {
             return partNumbers(input).values().stream()
+                    .parallel()
                     .flatMap(v -> v.stream())
-                    .mapToInt(Integer::intValue)
+                    .mapToInt(PartNumber::value)
                     .sum();
         }
     }
@@ -22,8 +24,9 @@ public class Day03 {
     public static class Part2 {
         public static int execute(Path input) throws IOException {
             return partNumbers(input).entrySet().stream()
+                    .parallel()
                     .filter(e -> e.getKey().type() == GEAR && e.getValue().size() == 2)
-                    .mapToInt(e -> e.getValue().stream().mapToInt(Integer::intValue).reduce(1, Math::multiplyExact))
+                    .mapToInt(e -> e.getValue().stream().mapToInt(PartNumber::value).reduce(1, Math::multiplyExact))
                     .sum();
         }
     }
@@ -32,32 +35,38 @@ public class Day03 {
 
     private static final char SPACE = '.';
 
-    record Symbol(int row, int col, char type) {
+    private record Symbol(int row, int col, char type) {
 
     }
 
-    private static Map<Symbol, List<Integer>> partNumbers(Path input) throws IOException {
+    private record PartNumber(int value, Symbol symbol) {
+
+    }
+
+    private static Map<Symbol, List<PartNumber>> partNumbers(Path input) throws IOException {
         var schematic = parse(input);
-        var numbers = new HashMap<Symbol, List<Integer>>();
         int rows = schematic.length;
         int columns = schematic[0].length;
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < columns; col++) {
-                if (Character.isDigit(schematic[row][col])) {
-                    int number = 0;
-                    var start = col;
-                    while (col < columns && Character.isDigit(schematic[row][col])) {
-                        number = number * 10 + (schematic[row][col] - '0');
-                        col++;
+        return Stream.iterate(0, Math::incrementExact).limit(rows)
+                .parallel()
+                .flatMap(row -> {
+                    var partNumbers = new ArrayList<PartNumber>();
+                    for (int col = 0; col < columns; col++) {
+                        if (Character.isDigit(schematic[row][col])) {
+                            int number = 0;
+                            var start = col;
+                            while (col < columns && Character.isDigit(schematic[row][col])) {
+                                number = number * 10 + (schematic[row][col] - '0');
+                                col++;
+                            }
+                            var symbol = adjacentSymbol(schematic, row, start, col).orElse(null);
+                            if (symbol != null) {
+                                partNumbers.add(new PartNumber(number, symbol));
+                            }
+                        }
                     }
-                    var symbol = adjacentSymbol(schematic, row, start, col).orElse(null);
-                    if (symbol != null) {
-                        numbers.computeIfAbsent(symbol, k -> new ArrayList<>()).add(number);
-                    }
-                }
-            }
-        }
-        return numbers;
+                    return partNumbers.stream();
+                }).collect(Collectors.groupingBy(PartNumber::symbol));
     }
 
     private static Optional<Symbol> adjacentSymbol(char[][] schematic, int currentRow, int start, int end) {
