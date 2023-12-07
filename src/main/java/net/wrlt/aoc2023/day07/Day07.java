6328b6ce699e48826ad6c4e5dd3f63c0d5197665
Juan Parera <1420988+jparera@users.noreply.github.com>
2023-12-07 15:43:29 +0100

    Enhance readability of Day 7 puzzle solution

--- src/main/java/net/wrlt/aoc2023/day07/Day07.java
+++ src/main/java/net/wrlt/aoc2023/day07/Day07.java
@@ -10,14 +10,14 @@ public class Day07 {
         private static char[] CARD_SET = { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
 
         public static long execute(Path input) throws IOException {
-            return winnings(input, Scale.ofCards(CARD_SET, Part1::cardFrequencies));
+            var scale = Scale.ofCards(CARD_SET, Part1::cardFrequencies);
+            return winnings(input, scale);
         }
 
-        private static int[] cardFrequencies(String cards, int[] map) {
+        private static int[] cardFrequencies(char[] cards) {
             var f = new int[CARD_SET.length];
-            for (int i = 0; i < cards.length(); i++) {
-                var card = map[cards.charAt(i)];
-                f[card]++;
+            for (int i = 0; i < cards.length; i++) {
+                f[cards[i]]++;
             }
             return f;
         }
@@ -26,23 +26,24 @@ public class Day07 {
     public static class Part2 {
         private static final char[] CARD_SET = { 'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A' };
 
-        private static final char JOKER = 'J';
+        private static final char JOKER_INDEX = 0;
 
         public static int execute(Path input) throws IOException {
-            return winnings(input, Scale.ofCards(CARD_SET, Part2::cardFrequencies));
+            var scale = Scale.ofCards(CARD_SET, Part2::cardFrequencies);
+            return winnings(input, scale);
         }
 
-        private static int[] cardFrequencies(String cards, int[] map) {
+        private static int[] cardFrequencies(char[] cards) {
             var f = new int[CARD_SET.length];
             int max = Integer.MIN_VALUE;
             int maxIndex = 0;
-            var jokers = 0;
-            for (int i = 0; i < cards.length(); i++) {
-                if (cards.charAt(i) == JOKER) {
+            int jokers = 0;
+            for (int i = 0; i < cards.length; i++) {
+                if (cards[i] == JOKER_INDEX) {
                     jokers++;
                     continue;
                 }
-                var card = map[cards.charAt(i)];
+                var card = cards[i];
                 f[card]++;
                 if (f[card] > max) {
                     max = f[card];
@@ -56,9 +57,9 @@ public class Day07 {
 
     private static int winnings(Path input, Scale scale) throws IOException {
         try (var lines = parse(input)) {
-            var sorted = lines.map(line -> Hand.valueOf(line, scale)).sorted();
-            var it = sorted.iterator();
+            var sorted = lines.map(line -> valueOf(line, scale)).sorted();
             int sum = 0;
+            var it = sorted.iterator();
             for (int i = 1; it.hasNext(); i++) {
                 var hand = it.next();
                 sum += i * hand.bid();
@@ -67,50 +68,38 @@ public class Day07 {
         }
     }
 
-    private static class Hand implements Comparable<Hand> {
-        private String cards;
+    public static Hand valueOf(String line, Scale scale) {
+        var parts = line.split("\\s");
+        var cards = scale.normalize(parts[0]);
+        var bid = Integer.parseInt(parts[1]);
+        var weight = scale.weight(cards);
+        return new Hand(bid, weight);
+    }
 
-        private int bid;
-
-        private int weight;
-
-        public Hand(String cards, int bid, int weight) {
-            this.cards = cards;
-            this.weight = weight;
-            this.bid = bid;
-        }
-
-        public int bid() {
-            return bid;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%s %s", cards, bid);
-        }
-
+    private record Hand(int bid, int weight) implements Comparable<Hand> {
         @Override
         public int compareTo(Hand o) {
             return Integer.compareUnsigned(weight, o.weight);
         }
-
-        public static Hand valueOf(String line, Scale scale) {
-            var parts = line.split("\\s");
-            var cards = parts[0];
-            var bid = Integer.parseInt(parts[1]);
-            var weight = scale.weight(cards);
-            return new Hand(parts[0], bid, weight);
-        }
     }
 
     private static class Scale {
-        private final int[] map;
+        private final char[] cardSetMap;
 
         private final CardFrequencies cardFrequencies;
 
-        public Scale(int[] map, CardFrequencies cardFrequencies) {
-            this.map = map;
+        public Scale(char[] cardSetMap, CardFrequencies cardFrequencies) {
+            this.cardSetMap = cardSetMap;
             this.cardFrequencies = cardFrequencies;
+        }
+
+        public char[] normalize(String cards) {
+            var length = cards.length();
+            var buffer = new char[length];
+            for (int i = 0; i < length; i++) {
+                buffer[i] = cardSetMap[cards.charAt(i)];
+            }
+            return buffer;
         }
 
         public static Scale ofCards(char[] cardSet, CardFrequencies frequencies) {
@@ -118,29 +107,29 @@ public class Day07 {
             if (cardSet.length > 16) {
                 throw new IllegalArgumentException();
             }
-            var map = new int[256];
+            var cardSetMap = new char[256];
             for (int i = 0; i < cardSet.length; i++) {
-                map[cardSet[i]] = i;
+                cardSetMap[cardSet[i]] = (char) i;
             }
-            return new Scale(map, frequencies);
+            return new Scale(cardSetMap, frequencies);
         }
 
-        public int weight(String cards) {
+        public int weight(char[] cards) {
             // 4 bits = 16 -> 4 * 1 type + 4 * 5 cards = 24 bits < 32
-            if (cards.length() > 5) {
+            if (cards.length > 5) {
                 throw new IllegalArgumentException();
             }
             int weight = typeWeight(cards);
-            for (int i = 0; i < cards.length(); i++) {
+            for (int i = 0; i < cards.length; i++) {
                 weight <<= 4;
-                weight += map[cards.charAt(i)];
+                weight += cards[i];
             }
             return weight;
         }
 
-        private int typeWeight(String cards) {
-            var f = cardFrequencies.count(cards, map);
-            var ff = new int[cards.length() + 1];
+        private int typeWeight(char[] cards) {
+            var f = cardFrequencies.count(cards);
+            var ff = new int[cards.length + 1];
             for (int i = 0; i < f.length; i++) {
                 ff[f[i]]++;
             }
@@ -170,7 +159,7 @@ public class Day07 {
 
         @FunctionalInterface
         private interface CardFrequencies {
-            int[] count(String cards, int[] map);
+            int[] count(char[] cards);
         }
     }
 
